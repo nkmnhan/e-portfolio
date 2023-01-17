3255e095944c4ae4c7bc270df836339ec9d3b4da
nkmnhan <nkmnhan@gmail.com>
2023-01-17 17:34:51 +0700

    secure nodejs with keycloak

--- microservices/apps/portfolio-api/src/main.ts
+++ microservices/apps/portfolio-api/src/main.ts
@@ -6,11 +6,33 @@
 import * as express from 'express';
 import * as path from 'path';
 
+const session = require('express-session');
+const Keycloak = require('keycloak-connect');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const memoryStore = new session.MemoryStore();
+const keycloak = new Keycloak({ store: memoryStore });
+
 const app = express();
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(
+  session({
+    secret: 'my-session-secret',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore,
+  })
+);
+
+app.use(keycloak.middleware());
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api', (req, res) => {
+app.get('/api', keycloak.protect(), (req, res) => {
   res.send({ message: 'Welcome to portfolio-api!' });
 });
 
