1738fd30db19a7d951222fa35d67a146a36e3f3f
nkmnhan <nkmnhan@gmail.com>
2023-02-15 20:37:14 +0700

    Update package

--- my-sst-app/stacks/MyStack.ts
+++ my-sst-app/stacks/MyStack.ts
@@ -11,13 +11,15 @@ export function API({ stack }: StackContext) {
   // });
   const site = new NextjsSite(stack, "my-nextjs-site", {
     path: "my-nextjs-app/",
+    runtime: "nodejs18.x",
     customDomain: {
       domainName: "nkmnhan.com",
       domainAlias: "www.nkmnhan.com",
+      hostedZone: "nkmnhan.com"
     },
   });
 
   stack.addOutputs({
-    URL: site.url,
+    URL: site.url || site.customDomainUrl || "Error"
   });
 }
