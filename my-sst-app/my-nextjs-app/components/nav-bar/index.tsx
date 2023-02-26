47652873546f41d1802b180658c04839e0b796ea
nkmnhan <nkmnhan@gmail.com>
2023-02-26 23:43:32 +0700

    Use next/link

--- my-sst-app/my-nextjs-app/components/nav-bar/index.tsx
+++ my-sst-app/my-nextjs-app/components/nav-bar/index.tsx
@@ -1,24 +1,28 @@
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, RocketLaunchIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Fragment } from 'react'
 import ThemeSwitcher from '../theme-switcher'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export type NavigationProps = {
   name: string,
-  href: string,
-  current: boolean
+  href: string
 }
 
 const navigation: NavigationProps[] = [
-  { name: 'About', href: '/about', current: true },
-  { name: 'Contact', href: '/contact', current: false },
+  { name: 'About', href: '/about' },
+  { name: 'Contact', href: '/contact' },
 ]
+
 
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ')
 }
 
 export default function NavBar() {
+  let currentPage = useRouter().pathname;
+
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -26,7 +30,7 @@ export default function NavBar() {
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
+                {/* Mobile menu button: XMark or 3Bars button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -36,39 +40,33 @@ export default function NavBar() {
                   )}
                 </Disclosure.Button>
               </div>
+
+              {/* Desktop NavBar */}
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="block h-8 w-auto lg:hidden"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
-                  <img
-                    className="hidden h-8 w-auto lg:block"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
+                  <RocketLaunchIcon className="h-6 w-6" aria-hidden="true" />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a
+                      <Link
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          currentPage.startsWith(item.href) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                           'px-3 py-2 rounded-md text-sm font-medium'
                         )}
-                        aria-current={item.current ? 'page' : undefined}
+                        aria-current={currentPage.startsWith(item.href) ? 'page' : undefined}
                       >
                         {item.name}
-                      </a>
+                      </Link>
                     ))}
                   </div>
                 </div>
               </div>
+
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <ThemeSwitcher/>
+                <ThemeSwitcher />
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
@@ -133,18 +131,17 @@ export default function NavBar() {
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pt-2 pb-3">
               {navigation.map((item) => (
-                <Disclosure.Button
+                <Link
                   key={item.name}
-                  as="a"
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    currentPage.startsWith(item.href) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                     'block px-3 py-2 rounded-md text-base font-medium'
                   )}
-                  aria-current={item.current ? 'page' : undefined}
+                  aria-current={currentPage.startsWith(item.href) ? 'page' : undefined}
                 >
                   {item.name}
-                </Disclosure.Button>
+                </Link>
               ))}
             </div>
           </Disclosure.Panel>
