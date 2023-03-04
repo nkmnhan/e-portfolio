a0c61abdb50b4de3db218d0b00b3d2a6fa150014
nkmnhan <nkmnhan@gmail.com>
2023-03-05 00:43:56 +0700

    Excute Prettier

--- my-sst-app/my-nextjs-app/components/hamburgur/index.tsx
+++ my-sst-app/my-nextjs-app/components/hamburgur/index.tsx
@@ -17,11 +17,10 @@ export default function HamburgerBtn({ active }: HamburgerBtnProp) {
 function Bar3() {
   return (
     <span
-      className=" absolute bottom-0 -mt-px block h-2 w-10 bg-[url('/nav-hamburger-bot.png')] bg-cover invert transition delay-75  ease-in
-before:absolute before:-top-6 before:-mt-px before:block
+      className="absolute bottom-0 -mt-px block h-2 w-10 bg-[url('/nav-hamburger-bot.png')] bg-cover invert transition delay-75 ease-in
+      before:absolute before:-top-6 before:-mt-px before:block
       before:h-2 before:w-10 before:bg-[url('/nav-hamburger-bot.png')] before:bg-cover before:transition before:delay-75 before:ease-in
-      after:absolute after:-top-3 after:-mt-px after:block
-after:h-2 after:w-10 after:bg-[url('/nav-hamburger-bot.png')] after:bg-cover after:transition after:delay-75  after:ease-in"
+      after:absolute after:-top-3 after:-mt-px after:block after:h-2 after:w-10 after:bg-[url('/nav-hamburger-bot.png')] after:bg-cover after:transition after:delay-75 after:ease-in"
     ></span>
   );
 }
@@ -30,12 +29,8 @@ function BarX() {
   return (
     <span
       className="absolute bottom-3 -mt-px block h-2 w-10 -rotate-45 bg-[url('/nav-hamburger-bot.png')] bg-cover invert transition delay-75 ease-in
-
-  before:absolute before:-mt-px before:block
-  before:h-2  before:w-10 before:-rotate-90 before:bg-[url('/nav-hamburger-bot.png')] before:bg-cover before:transition before:delay-75 before:ease-in
-
-  after:absolute after:-mt-px after:block
-after:h-2  after:w-10 after:rotate-45 after:bg-[url('/nav-hamburger-bot.png')] after:bg-cover after:opacity-0 after:transition after:delay-75 after:ease-in"
+      before:absolute before:-mt-px before:block before:h-2  before:w-10 before:-rotate-90 before:bg-[url('/nav-hamburger-bot.png')] before:bg-cover before:transition before:delay-75 before:ease-in
+      after:absolute after:-mt-px after:block after:h-2  after:w-10 after:rotate-45 after:bg-[url('/nav-hamburger-bot.png')] after:bg-cover after:opacity-0 after:transition after:delay-75 after:ease-in"
     ></span>
   );
 }
