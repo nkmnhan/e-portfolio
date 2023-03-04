8b487809778b71ff7aab67d32eb370c8769c626b
nkmnhan <nkmnhan@gmail.com>
2023-03-04 22:39:35 +0700

    Add logo

---
+++ my-sst-app/my-nextjs-app/components/logo/index.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <>
+      <img src="/home-logo.png" className=" lg:w-72 lg:h-52 sm:h-32"></img>
+    </>
+  );
+}
--- my-sst-app/my-nextjs-app/pages/index.tsx
+++ my-sst-app/my-nextjs-app/pages/index.tsx
@@ -1,14 +1,24 @@
+import Logo from "@/components/logo";
+
 const HomePage = () => {
   return (
     <>
-    <div className="bg-none bg-cover bg-cent fixed h-screen w-full -z-50">
-      <div className=" -z-50 absolute top-0 w-full h-full">
-       <video autoPlay loop muted>
-        <source src="https://mindbender.com/content/videos/starting-page-video.mp4" type="video/mp4"></source>
-       </video>
+      <div className="bg-center fixed -z-50 h-screen w-full bg-red-900 bg-none bg-cover">
+        <div className="absolute top-0 -z-50 hidden h-full w-full lg:block">
+          <video autoPlay loop muted className=" min-w-full min-h-full">
+            <source
+              src="https://mindbender.com/content/videos/starting-page-video.mp4"
+              type="video/mp4"
+            ></source>
+          </video>
+        </div>
+        <div className="absolute top-0 h-full w-full bg-[url('/home-dots.png')]  bg-[length:3px_3px]"></div>
       </div>
-      <div className="absolute top-0 bg-[url('/home-dots.png')] h-full w-full  bg-[length:3px_3px]">
-      </div></div>
+      <div className=" fixed top-0 left-0 w-full h-screen -z-40">
+        <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Logo/>
+        </div>
+      </div>
     </>
   );
 };
