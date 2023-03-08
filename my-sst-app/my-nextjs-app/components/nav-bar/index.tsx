fd6e9b287a4f51b65f22c5cd504505bd19636d64
nkmnhan <nkmnhan@gmail.com>
2023-03-08 21:13:10 +0700

    Add social contact URLs

--- my-sst-app/my-nextjs-app/components/nav-bar/index.tsx
+++ my-sst-app/my-nextjs-app/components/nav-bar/index.tsx
@@ -2,22 +2,38 @@ import { Popover, Transition } from "@headlessui/react";
 import Link from "next/link";
 import { Fragment } from "react";
 import HamburgerBtn from "../hamburgur";
+import InstagramIco from "../icons/instagram";
+import GithhubIco from "../icons/github";
+import LinkedInIco from "../icons/linkedin";
 
 export type NavigationProps = {
+  id: string;
   name: string;
   href: string;
+  icon?: JSX.Element;
 };
 
 const navigations: NavigationProps[] = [
-  { name: "Home", href: "/" },
-  { name: "About", href: "/about" },
-  { name: "Gallary", href: "/gallary" },
-  { name: "Contact", href: "/contact" },
+  { id: "home", name: "Home", href: "/" },
+  { id: "about", name: "About", href: "/about" },
+  { id: "gallary", name: "Gallary", href: "/gallary" },
+  { id: "contact", name: "Contact", href: "/contact" },
+];
+
+const socialContacts: NavigationProps[] = [
+  { id: "instagram", name: "Instagram", href: "#", icon: InstagramIco() },
+  {
+    id: "github",
+    name: "Github",
+    href: "https://github.com/nkmnhan",
+    icon: GithhubIco(),
+  },
+  { id: "linkedin", name: "LinkedIn", href: "##", icon: LinkedInIco() },
 ];
 
 export default function NavBar() {
   return (
-    <div className="fixed top-4 left-4 w-full max-w-sm md:top-12 md:left-20 z-50">
+    <div className="fixed top-4 left-4 z-50 w-full max-w-sm md:top-8 md:left-8">
       <Popover className="relative">
         {({ open }) => (
           /* Use the `open` state to conditionally change the direction of the chevron icon. */
@@ -34,18 +50,33 @@ export default function NavBar() {
               leaveFrom="opacity-100 translate-x-1"
               leaveTo="opacity-0 translate-x-0"
             >
-              <Popover.Panel className="max-w-sms absolute z-10 w-full">
+              <Popover.Panel className="absolute z-50 mt-3 w-screen max-w-sm pr-8 sm:px-0 md:px-4">
                 {({ close }) => (
-                  <div className="overflow-hidden">
-                    <div className="relative grid pr-10 font-bold text-white md:pr-0 md:pt-7">
+                  <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                    <div className="relative grid grid-cols-2 gap-2 bg-white p-2 md:grid-cols-1">
                       {navigations.map((item) => (
                         <Link
-                          key={item.href}
-                          className="my-0 flex w-full items-center border-b border-white p-2 transition delay-75 duration-150 ease-in-out first:border-t focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                          id={item.id}
                           href={item.href}
+                          className="flex w-full items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none"
                           onClick={() => close()}
                         >
-                          {item.name}
+                          <div className="ml-4">
+                            <p className="text-sm font-medium text-gray-900">
+                              {item.name}
+                            </p>
+                          </div>
+                        </Link>
+                      ))}
+                    </div>
+                    <div className="flex flex-row bg-gray-50 p-4">
+                      {socialContacts.map((item) => (
+                        <Link
+                          id={item.id}
+                          href={item.href}
+                          className=" flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none"
+                        >
+                          <div className=" h-6 w-6">{item.icon}</div>
                         </Link>
                       ))}
                     </div>
