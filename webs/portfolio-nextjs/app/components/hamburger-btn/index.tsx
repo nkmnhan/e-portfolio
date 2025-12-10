877b40d94fb751c7cb82acd35e7de03af12775fe
nkmnhan <49507410+nkmnhan@users.noreply.github.com>
2025-12-10 22:12:01 +0700

    feat: Add HamburgerIcon component for improved button icon rendering; refactor HamburgerBtn to utilize new icon component

--- webs/portfolio-nextjs/app/components/hamburger-btn/index.tsx
+++ webs/portfolio-nextjs/app/components/hamburger-btn/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from "flowbite-react";
 import { clsxMerge } from "@/app/components/themes/utils";
+import HamburgerIcon from "./hamburger-icon";
 
 export type HamburgerBtnProp = {
   id?: string;
@@ -36,49 +37,7 @@ export default function HamburgerBtn({
       aria-pressed={active ? "true" : "false"}
       onClick={onClick}
     >
-      <span className="sr-only">Menu</span>
-      <svg
-        className={clsxMerge(btnSize, "fill-current pointer-events-none")}
-        viewBox="0 0 16 16"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect
-          className={clsxMerge(
-            "origin-center",
-            "-translate-y-1", // 4px up
-            "translate-x-2",  // 8px right
-            "transition-all duration-500 ease-[cubic-bezier(.5,.85,.25,1.1)]",
-            "group-aria-pressed:translate-x-0 group-aria-pressed:translate-y-0 group-aria-pressed:rotate-315"
-          )}
-          y="7"
-          width="9"
-          height="2"
-          rx="1"
-        ></rect>
-        <rect
-          className={clsxMerge(
-            "origin-center",
-            "transition-all duration-500 ease-[cubic-bezier(.5,.85,.25,1.8)]",
-            "group-aria-pressed:rotate-45"
-          )}
-          y="7"
-          width="16"
-          height="2"
-          rx="1"
-        ></rect>
-        <rect
-          className={clsxMerge(
-            "origin-center",
-            "translate-y-1", // 4px down
-            "transition-all duration-500 ease-[cubic-bezier(.5,.85,.25,1.1)]",
-            "group-aria-pressed:translate-y-0 group-aria-pressed:rotate-135"
-          )}
-          y="7"
-          width="9"
-          height="2"
-          rx="1"
-        ></rect>
-      </svg>
+      <HamburgerIcon active={active} mode={mode} />
     </Button>
   );
 }
