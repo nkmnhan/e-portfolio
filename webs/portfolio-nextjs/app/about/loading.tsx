b70e02b807ee99edb976f6c9c69ced45c9e88bcc
nkmnhan <49507410+nkmnhan@users.noreply.github.com>
2025-12-23 22:09:34 +0700

    feat: refactor loading animations and update image paths for About page

--- webs/portfolio-nextjs/app/about/loading.tsx
+++ webs/portfolio-nextjs/app/about/loading.tsx
@@ -1,20 +1,32 @@
 import { clsxMerge } from "../components/themes";
 import AboutLogo from "./about-logo";
-import "./about.css"
+
 export default function LoadingAbout() {
   return (
-    <main className="absolute w-full h-full bg-[#020016] loading-shrink pointer-events-none">
+    <main
+      className={clsxMerge(
+        "bg-[#020016]",
+        "absolute w-full h-full z-400",
+        "animate-shrink-out origin-top animation-delay-2000 [animation-fill-mode:forwards] pointer-events-none"
+      )}
+    >
       <div
         className={clsxMerge(
           "absolute inset-0 flex items-center justify-center",
-          "fade-out-after-2s"
+          "animate-fade-out animation-delay-1750 [animation-fill-mode:forwards]"
         )}
       >
         <div className="w-60 flex flex-col items-center gap-6">
           <AboutLogo />
           <div className="w-full">
-            <div className="relative h-2 w-full overflow-hidden rounded bg-gray-200">
-              <div className="absolute left-0 top-0 h-2 w-1/3 bg-[#cb7f65] animate-loading-bar"></div>
+            <div className="relative h-1 w-full overflow-hidden rounded bg-gray-200">
+              <div
+                className={clsxMerge(
+                  "absolute left-0 top-0 h-1 w-1/3",
+                  "bg-[#cb7f65]",
+                  "animate-loading-bar"
+                )}
+              ></div>
             </div>
           </div>
         </div>
