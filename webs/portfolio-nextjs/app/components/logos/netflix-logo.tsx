50c1a99a578fdb137a5b92479f1aba7c0bb9d892
ORIENTSOFTWARE\nhan.nguyen <nhan.nguyen@orientsoftware.com>
2026-01-06 23:35:42 +0700

    Refactor code structure for improved readability and maintainability

--- webs/portfolio-nextjs/app/components/logos/netflix-logo.tsx
+++ webs/portfolio-nextjs/app/components/logos/netflix-logo.tsx
@@ -3,31 +3,32 @@ import "./netflix-logo.css";
 
 export default function NetFlixLogo() {
   return (
-    <>
-      <div className="relative flex items-start justify-center h-60">
-        {/* left vertical stroke */}
-        <div className="relative w-12 h-full n-col">
-          <div className="n-fill n-fill--left" />
-        </div>
-
-        {/* diagonal stroke (middle) */}
-        <div className="relative w-12 h-full n-diagonal">
-          <div className="n-fill n-fill--middle" />
-        </div>
-
-        {/* right vertical stroke + vertical label */}
-        <div className="relative w-12 h-full n-col">
-          <div className="n-fill n-fill--right" />
-          <div
-            className={clsxMerge(
-              "n-label h-full w-full pt-4 flex justify-start items-center bg-transparent",
-              "walt-disney text-xl text-white font-bold [writing-mode:vertical-rl]"
-            )}
-          >
-            <span>Tony F. Wilson</span>
-          </div>
-        </div>
+    <div className="relative flex items-start justify-center">
+      <div
+        key={"left"}
+        className={clsxMerge("h-60 w-12 bg-red-800 scale-up")}
+      />
+      <div
+        key={"diagonal"}
+        className={clsxMerge(
+          "relative z-10 h-60 w-12 draw-diagonal -left-12",
+          "before:block",
+          "before:h-[140%]",
+          "before:w-full",
+          "before:bg-red-600",
+          "before:shadow-[0_0_30px_black]"
+        )}
+      />
+      <div
+        key={"right"}
+        className={clsxMerge(
+          "h-61 w-12 flex justify-start items-center",
+          "walt-disney text-xl text-white font-bold [writing-mode:vertical-rl]"
+        )}
+      >
+        <div className={clsxMerge("h-full w-full bg-red-800",  "[animation-delay:1.2s] scale-up")} />
+        <span className="absolute z-2 pt-4">Tony F. Wilson</span>
       </div>
-    </>
+    </div>
   );
 }
